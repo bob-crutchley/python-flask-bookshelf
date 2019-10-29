9ad4ab87611ef51717b89b495f1f7043a7455bf6
bob-crutchley <bob.crutchley96@gmail.com>
2019-10-29 16:27:10 +0000

    packaging account

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
 from setuptools import find_packages, setup
 
+with open("VERSION", "r") as version_file:
+    version = version_file.read().strip()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='flask-test',
-    version='0.1',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
+    name="flask_test",
+    version=version,
+    author="Bob",
+    author_email="bob@emai.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=find_packages("src"),
+    package_dir={"":"src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
     install_requires=[
         'flask',
-	'gunicorn'
-    ],
+        'gunicorn'
+    ]
 )
 
